6cbc5cbc92b84becca2478e9537420554c37f723
brigit3321 <qqq44cm9@yahoo.co.jp>
2021-02-08 19:36:29 +0900

    messagesテーブルの作成

---
+++ app/models/message.rb
@@ -0,0 +1,13 @@
+class CreateMessages < ActiveRecord::Migration[6.0]
+    belongs_to :room
+    belongs_to :user
+    
+    def change
+      create_table :messages do |t|
+        t.string  :content
+        t.references :room, foreign_key: true
+        t.references :user, foreign_key: true
+        t.timestamps
+      end
+    end
+  end
